6497784ca7c7bb673b6f845900fd2576e6f5794a
ShanmukhaManoj11 <shanmukhamanoj007@gmail.com>
2019-03-12 10:36:49 -0400

    JMT trajectory generation

--- modules/planning/planner/sample/sample_planner.cc
+++ modules/planning/planner/sample/sample_planner.cc
@@ -52,6 +52,44 @@ using apollo::common::math::CartesianFrenetConverter;
 using apollo::common::math::PathMatcher;
 using apollo::common::time::Clock;
 
+/*
+method to convert reference points on reference line to a vector of path points
+*/
+std::vector<PathPoint> ToDiscretizedReferenceLine(const std::vector<ReferencePoint>& ref_points){
+  double s = 0.0;
+  std::vector<PathPoint> path_points;
+  for (const auto& ref_point : ref_points) {
+    PathPoint path_point;
+    path_point.set_x(ref_point.x());
+    path_point.set_y(ref_point.y());
+    path_point.set_theta(ref_point.heading());
+    path_point.set_kappa(ref_point.kappa());
+    path_point.set_dkappa(ref_point.dkappa());
+
+    if (!path_points.empty()) {
+      double dx = path_point.x() - path_points.back().x();
+      double dy = path_point.y() - path_points.back().y();
+      s += std::sqrt(dx * dx + dy * dy);
+    }
+    path_point.set_s(s);
+    path_points.push_back(std::move(path_point));
+  }
+  return path_points;
+}
+
+void ComputeInitFrenetState(const PathPoint& matched_point,
+                            const TrajectoryPoint& cartesian_state,
+                            std::array<double, 3>* ptr_s,
+                            std::array<double, 3>* ptr_d) {
+  CartesianFrenetConverter::cartesian_to_frenet(
+      matched_point.s(), matched_point.x(), matched_point.y(),
+      matched_point.theta(), matched_point.kappa(), matched_point.dkappa(),
+      cartesian_state.path_point().x(), cartesian_state.path_point().y(),
+      cartesian_state.v(), cartesian_state.a(),
+      cartesian_state.path_point().theta(),
+      cartesian_state.path_point().kappa(), ptr_s, ptr_d);
+}
+
 Status SamplePlanner::Plan(const TrajectoryPoint& planning_start_point,
                             Frame* frame,
                             ADCTrajectory* ptr_computed_trajectory) {
@@ -61,6 +99,24 @@ Status SamplePlanner::Plan(const TrajectoryPoint& planning_start_point,
 Status SamplePlanner::PlanOnReferenceLine(
     const TrajectoryPoint& planning_init_point, Frame* frame,
     ReferenceLineInfo* reference_line_info) {
+	reference_line_info->set_is_on_reference_line();
+	//1. reference points to vector of PathPoints
+	auto path_points = std::make_shared<std::vector<PathPoint>>(ToDiscretizedReferenceLine(
+		reference_line_info->reference_line().reference_points()));
+	//2. match planning_init_point to the vector of PathPoints
+	PathPoint matched_point=PathMatcher::MatchToPath(*path_points,planning_init_point.path_point().x(),
+		planning_init_point.path_point().y());
+	//3. convert planning_init_point to frenet frame according to matched point
+	std::array<double,3> init_s;
+	std::array<double,3> init_d;
+	ComputeInitFrenetState(matched_point,planning_init_point,&init_s,&init_d);
+	//4. get obstacles on reference line and compute target frenet coordinates
+	std::array<double,3> target_s;
+	std::array<double,3> target_d={0.0,0.0,0.0};
+	
+	//5. compute JMTs (5th order polynomials) for s and d independently from init and target frenet coordinates
+
+	//6. convert frenet s,d points from JMTs back to xy coordinates
   return Status::OK();
 }
 
